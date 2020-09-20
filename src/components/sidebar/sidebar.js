869fdc178b0b506c7af937da43d07deac128e8e4
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-09-20 21:36:36 +0700

    adding list sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,20 +1,25 @@
 import React from 'react';
-// import bootstrap
-import {Link} from 'react-router-dom';
+import './Sidebar.css'
+// import react router dom
+import {Link} from "react-router-dom";  
 
-const SideBar = () =>{
-    return(
-        <div>
+
+
+  
+  export default function SidebarExample() {
+    return (
+        <div className="sidebar">
             <ul>
-                <li>
-                    <Link to="/week1-day1">Week 1 Day 1</Link>
-                </li>
-                <li>
-                    <Link to="/week1-day2">Week 1 Day 2</Link>
-                </li>
+            <li>
+                <Link to="/Exercise/Week1day1">Week 1 day 1</Link>
+            </li>
+            <li>
+                <Link to="/Exercise/Week1day2">Weed 1 day 2</Link>
+            </li>
+            <li>
+                <Link to="/Exercise/Week1day3">Week 1 day 2</Link>
+            </li>
             </ul>
-        </div>
+      </div>
     )
-}
-
-export default SideBar;
+  }  
