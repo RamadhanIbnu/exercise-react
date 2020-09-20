e9b6818356f73744e577196dfc94180f27ed0b5d
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-09-20 21:59:42 +0700

    fix error for deploy netflify

--- src/pages/exercise.js
+++ src/pages/exercise.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Exercise.css"
-//import bootstrap
-import { Container,Row,Col } from "react-bootstrap";
 import SideBar from '../components/sidebar/sidebar';
 // import react router dom
 import {
